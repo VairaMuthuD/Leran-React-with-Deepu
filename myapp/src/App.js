d5368bd8486edc0d3dd11cee6f4bafa76b2de272
DeepuVaira <deepikavairamuthu@gmail.com>
2024-03-12 08:39:36 +0530

    Js files added

---
+++ myapp/src/App.js
@@ -0,0 +1,33 @@
+import {useState} from 'react';
+import './App.css';
+
+function App() {
+
+  const[count, setCount] = useState(0);
+
+  function handleChange(){
+    setCount(prev => prev + 1)
+  }
+
+  function handleChange1(){
+    setCount(prev => prev > 0 ? prev - 1 : prev )
+  }
+
+  return (
+    <div className="App">
+      <div>
+      <button onClick={handleChange}>
+          Plus
+       </button>
+
+       <button onClick={handleChange1}>
+          Minus
+       </button>
+      </div>
+      
+       <div>{count}</div>
+    </div>
+  );
+}
+
+export default App;
